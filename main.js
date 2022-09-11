f481590d09859003c0029e0548f6ee8c554174e1
Andre Miranda <andre@andreldm.com>
2022-09-12 01:32:15 +0200

    Add barebones menu with routes

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
+import Menu from './components/Menu.js';
 import App from './components/App.js';
 
-const app = Vue.createApp({
-  render: () => Vue.h(App),
+const routes = [
+  { path: '/', component: Menu },
+  { path: '/app', component: App }
+];
+
+const router = VueRouter.createRouter({
+  history: VueRouter.createMemoryHistory(),
+  routes
 });
+
+const app = Vue.createApp({});
+app.use(router);
 app.mount('#app');
