fd718b60da38919e8e18987a6e966f211d0d0866
Andre Miranda <andre@andreldm.com>
2022-09-20 01:22:22 +0200

    Improve hearts appearance

--- src/components/Lives.js
+++ src/components/Lives.js
@@ -6,9 +6,9 @@ export default {
     template:
         `
             <div class="hearts">
-                <span v-show="lives > 0" class="heart">&#9829;</span>
-                <span v-show="lives > 1" class="heart">&#9829;</span>
-                <span v-show="lives > 2" class="heart">&#9829;</span>
+                <span v-show="lives > 0" class="heart"></span>
+                <span v-show="lives > 1" class="heart"></span>
+                <span v-show="lives > 2" class="heart"></span>
             </div>
         `
 };
