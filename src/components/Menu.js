1e743c187d4e4bbb30f475124f1f3324fb473db1
Andre Miranda <andre@andreldm.com>
2022-09-20 01:20:04 +0200

    Add buttons to menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,12 +1,42 @@
 export default {
     name: 'Menu',
+    data() {
+      return {
+        mode: undefined
+      }
+    },
     methods: {
       start() {
         this.$router.push('/app')
       },
+      setMode(mode) {
+        this.mode = mode;
+      }
     },
     template:
         `
-            <button class="start-btn" v-on:click="start()">Start</button>
+            <span class="button-group">
+              <button class="button"
+                      v-bind:class="{ selected: mode == 'easy' }"
+                      v-on:click="setMode('easy')">
+                <div>Easy</div>
+                <div class="small">0-99</div>
+              </button>
+              <button class="button"
+                      v-bind:class="{ selected: mode == 'medium' }"
+                      v-on:click="setMode('medium')">
+                <div>Medium</div>
+                <div class="small">0-999</div>
+              </button>
+              <button class="button"
+                      v-bind:class="{ selected: mode == 'hard' }"
+                      v-on:click="setMode('hard')">
+                <div>Hard</div>
+                <div class="small">0-9999</div>
+              </button>
+            </span>
+            <button class="button"
+                    v-bind:disabled="mode == undefined"
+                    v-on:click="start()">Start</button>
         `
 };
