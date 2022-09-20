dbbcdd9a3a8be42fe24049980183f8bdab638e8b
Andre Miranda <andre@andreldm.com>
2022-09-20 21:17:18 +0200

    Animate to hearts when live decreases

--- components/Lives.js
+++ components/Lives.js
@@ -6,9 +6,9 @@ export default {
     template:
         `
             <div class="hearts">
-                <span v-show="lives > 0" class="heart"></span>
-                <span v-show="lives > 1" class="heart"></span>
-                <span v-show="lives > 2" class="heart"></span>
+                <span v-bind:class="{ broken: lives <= 0 }" class="heart"></span>
+                <span v-bind:class="{ broken: lives <= 1 }" class="heart"></span>
+                <span v-bind:class="{ broken: lives <= 2 }" class="heart"></span>
             </div>
         `
 };
