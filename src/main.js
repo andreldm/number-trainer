43a2ff8a84e5d47ead492d1ce6e9f93e65e84812
Andre Miranda <andre@andreldm.com>
2023-09-28 00:15:05 +0200

    Move assets out of source folder

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 
-import "./assets/main.css";
+import "./style/main.css";
 
 const app = createApp(App);
 app.use(router);
