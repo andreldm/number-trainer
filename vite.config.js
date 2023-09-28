82dc9514ba999d3e50027d88a73171b8b4d32843
Andre Miranda <andre@andreldm.com>
2023-09-28 17:29:44 +0200

    Initial configuration for PWA

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,14 @@ import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/number-trainer/",
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({ registerType: 'autoUpdate', injectRegister: 'auto' })],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
